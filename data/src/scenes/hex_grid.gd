e5e4ef0b924c3a88ffdff7ec4c568328d7f5e632
Alex Kraasch <alex@kraasch.eu>
2024-03-19 01:35:34 +0100

    Add perspective changes.

--- data/src/scenes/hex_grid.gd
+++ data/src/scenes/hex_grid.gd
@@ -1,6 +1,15 @@
 @tool
 extends Node3D
 
+const PERSPECTIVES : Array = [
+	[ Vector3(4, 4, 11), Vector3(-44.8,   0, 0) ],
+	[ Vector3(4, 4, -2), Vector3(-44.8, 210.5, 0) ],
+	[ Vector3(4, 6, 4), Vector3(-90, 90, 180) ],
+]
+
+var perspctives_index : int = 0
+var perspctives_max : int = len(PERSPECTIVES)
+
 const TILE_SIZE : float = Globals.EDGE_SIZE
 const HEX_TILE = preload("res://data/src/scenes/field.tscn")
 var grid_size : int = Globals.MAX_GRID_SIZE
@@ -25,8 +34,22 @@ func generate_grid():
 
 func handle_input():
 	if Input.is_action_just_pressed("my_cycle_next"):
-		pass
+		apply_perspective(1)
 	if Input.is_action_just_pressed("my_cycle_prev"):
-		pass
+		apply_perspective(-1)
 	if Input.is_action_just_pressed("my_cycle_center"):
 		pass
+
+func apply_perspective(step : int):
+	var new_perspective = PERSPECTIVES[perspctives_index]
+	var location = new_perspective[0]
+	var orientation = new_perspective[1]
+	# set values.
+	%Camera3D.position.x = location.x
+	%Camera3D.position.y = location.y
+	%Camera3D.position.z = location.z
+	%Camera3D.rotation.x = orientation.x
+	%Camera3D.rotation.y = orientation.y
+	%Camera3D.rotation.z = orientation.z
+	# increment index.
+	perspctives_index = (perspctives_index + step) % perspctives_max
