42889634d25a65a80e4b178540f26ea8ad4f8bee
Alex Kraasch <alex@kraasch.eu>
2024-03-20 18:45:56 +0100

    Fix bug: recalculate global within function.

--- data/src/scenes/field.gd
+++ data/src/scenes/field.gd
@@ -1,13 +1,10 @@
 @tool
 extends Node3D
 
-
 const CUBE_HEIGHT : float = 0.2
 const HEX_TILE = preload("res://data/src/scenes/hex_tile.tscn")
 const CUBE = preload("res://data/src/scenes/cube.tscn")
-var max_stack_height : int = Globals.MAX_STACK_HEIGHT
 var rng = RandomNumberGenerator.new()
-
 var is_debug : bool = false # NOTE: remove later. only for debugging colors.
 
 func _ready():
@@ -24,7 +21,8 @@ func generate_hex_tile(type_index : int):
 	add_child(tile)
 
 func generate_cube_stack(type_index : int):
-	var stack_height : int = rng.randf_range(0, max_stack_height + 1)
+	var max_stack_height : int = Globals.MAX_STACK_HEIGHT
+	var stack_height : int = rng.randf_range(0, max_stack_height)
 	for i in range(0, stack_height):
 		var cube = CUBE.instantiate()
 		cube.mymaterial = ColorGenerator.get_shader(type_index)
