93f26451cc30d552b51dd3763d1984d4dd64976b
Alex Kraasch <alex@kraasch.eu>
2024-03-22 21:13:59 +0100

    Add PERSPECTIVE mechanic.

--- data/src/scenes/main_scene.gd
+++ data/src/scenes/main_scene.gd
@@ -3,17 +3,29 @@ extends Node3D
 
 func _ready():
 	%EndButton.pressed.connect(self.exit_button_pressed)
+	%PreviousView.pressed.connect(self.previous_perspective)
+	%NextView.pressed.connect(self.next_perspective)
+	%ResetView.pressed.connect(self.reset_perspective)
+
+func _process(delta):
+	if Input.is_action_just_pressed("my_cycle_next"):
+		next_perspective()
+	if Input.is_action_just_pressed("my_cycle_prev"):
+		previous_perspective()
+	if Input.is_action_just_pressed("my_cycle_center"):
+		reset_perspective()
 
 func exit_button_pressed():
 	SceneManager.set_scene(preload("res://data/src/ui/end_screen.tscn"))
 
-func _process(delta):
-	if Input.is_action_just_pressed("my_cycle_next"):
-		apply_perspective(1)
-	if Input.is_action_just_pressed("my_cycle_prev"):
-		apply_perspective(-1)
-	if Input.is_action_just_pressed("my_cycle_center"):
-		pass
+func next_perspective():
+	apply_perspective(1)
+
+func previous_perspective():
+	apply_perspective(-1)
+
+func reset_perspective():
+	apply_perspective(0)
 
 const PERSPECTIVES : Array = [
 	[ Vector3(4, 4, 11), Vector3(-44.8,   0, 0) ],
@@ -21,11 +33,14 @@ const PERSPECTIVES : Array = [
 	[ Vector3(4, 6, 4), Vector3(-90, 90, 180) ],
 ]
 
-var perspctives_index : int = 0
+var current_perspctive_index : int = 0
 var perspctives_max : int = len(PERSPECTIVES)
 
 func apply_perspective(step : int):
-	var new_perspective = PERSPECTIVES[perspctives_index]
+	if step == 0:
+		current_perspctive_index = 0
+	var new_index : int = (current_perspctive_index + step) % perspctives_max
+	var new_perspective = PERSPECTIVES[new_index]
 	var location = new_perspective[0]
 	var orientation = new_perspective[1]
 	# set values.
@@ -36,4 +51,4 @@ func apply_perspective(step : int):
 	%Camera3D.rotation.y = orientation.y
 	%Camera3D.rotation.z = orientation.z
 	# increment index.
-	perspctives_index = (perspctives_index + step) % perspctives_max
+	current_perspctive_index = new_index
