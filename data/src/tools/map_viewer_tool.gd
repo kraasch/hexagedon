58b74a64096dc4b3e8a7b939fc6dd4c3a76ee2e7
Alex Kraasch <alex@kraasch.eu>
2024-03-24 12:44:46 +0100

    Rename GAME SCENE to MAIN SCENE.

--- data/src/tools/map_viewer_tool.gd
+++ data/src/tools/map_viewer_tool.gd
@@ -12,7 +12,7 @@ func _ready():
 var rng = RandomNumberGenerator.new()
 
 func play_button_pressed():
-	SceneManager.set_scene(preload("res://data/src/scenes/game_scene.tscn"))
+	SceneManager.set_scene(preload("res://data/src/scenes/main_scene.tscn"))
 
 func update_button_pressed():
 	Globals.NUM_OF_PARTIES     = %PartySliderDisplay.val
