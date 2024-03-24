fe64cf3d3345226944c9e20d840da9a80010ddb1
Alex Kraasch <alex@kraasch.eu>
2024-03-24 10:17:50 +0100

    Misc refactoring.

--- data/src/scenes/hex_tile.gd
+++ data/src/scenes/hex_tile.gd
@@ -69,6 +69,7 @@ func set_callback(_activate, _select, _deselect):
 	select   = _select
 	deselect = _deselect
 
+@warning_ignore("unused_parameter")
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
