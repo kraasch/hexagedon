ed95d90fcd699818e99c0adf1885eb45b238ac20
Alex Kraasch <alex@kraasch.eu>
2024-03-25 16:22:18 +0100

    For now quit the game after end screen.

--- data/src/ui/end_screen.gd
+++ data/src/ui/end_screen.gd
@@ -4,3 +4,4 @@ func _input(event):
 	if event.is_action_pressed('my_left_click'):
 		pass
 #		SceneManager.set_scene(preload("res://data/src/ui/main_menu.tscn"))
+		get_tree().quit() # TODO: remove later (for now quit the game here).
