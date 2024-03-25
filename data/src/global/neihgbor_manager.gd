acaf65a3a8bfb2c7e0674c078b9854e921273dbd
Alex Kraasch <alex@kraasch.eu>
2024-03-25 19:20:02 +0100

    Refactor.

--- data/src/global/neihgbor_manager.gd
+++ data/src/global/neihgbor_manager.gd
@@ -6,6 +6,7 @@ var scan_type : int = 0 # one of 4 ways to arrange a hexagon grid.
 var FIELD_NEIGHBORS : Array = []
 
 func is_neighbor(center_group_index : int, neighbor_group_index : int):
+	return false
 	return FIELD_NEIGHBORS[center_group_index].has(neighbor_group_index)
 
 func create_new_field_neighbors(length_val : int):
