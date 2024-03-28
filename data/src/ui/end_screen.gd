586736c13e59eab01dc3873a82a1283682ec5cc5
Alex Kraasch <alex@kraasch.eu>
2024-03-28 01:57:53 +0100

    Update start and end screen.

--- data/src/ui/end_screen.gd
+++ data/src/ui/end_screen.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-func _on_label_gui_input(event):
+func _on_color_rect_gui_input(event):
 	if event.is_action_pressed('my_left_click'):
 		pass
 #		SceneManager.set_scene(preload("res://data/src/ui/main_menu.tscn"))
