4f88e6ad8868431018c1e787c7953cfc37dd6323
Alex Kraasch <alex@kraasch.eu>
2024-04-11 23:14:33 +0200

    After round load main menu again.

--- data/src/ui/end_screen.gd
+++ data/src/ui/end_screen.gd
@@ -2,6 +2,5 @@ extends CanvasLayer
 
 func _on_color_rect_gui_input(event):
 	if event.is_action_pressed('my_left_click'):
-		pass
-#		SceneManager.set_scene(preload("res://data/src/ui/main_menu.tscn"))
-		get_tree().quit() # TODO: remove later (for now quit the game here).
+		SceneManager.set_scene(load("res://data/src/ui/main_menu.tscn"))
+		# TODO: properly unload all previous game state and objects.
